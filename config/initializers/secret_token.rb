ac5ad106004f45c5ec55c27671e3a3bdaff3eed8
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-10-08 10:08:51 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hummer::Application.config.secret_token = '6e4759fddaea2e8241cb72ab68929efdcfd5cbb93a48e43a4913bc27a6874aea2ac6875cf57b8c6cbb25a97062f9877929a53c1480ffb4c29518e9ad6fd9cbe2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hummer::Application.config.session_store :cookie_store, :key => '_Hummer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hummer::Application.config.session_store :active_record_store

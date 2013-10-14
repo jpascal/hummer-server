8b401bc99130381f97ba702687266b52afcb8874
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-10-14 17:31:38 +0400

    fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def update
     @user.update(edit_user)
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to :back
     else
       render :edit
     end

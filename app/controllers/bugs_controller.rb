4ff0e9548547ce388768642eecbce9c3179ad134
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-10-18 18:00:08 +0400

    add tabs to bug index page

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,6 +1,13 @@
 class BugsController < ApplicationController
   def index
-    @bugs = Case.where("tracker IS NOT NULL").page(params[:page])
+    @bugs = Case.where("tracker IS NOT NULL").includes(:result)
+    @total_tests = @bugs.count
+    @total_errors = @bugs.where(:results => {:type => "error"}).count
+    @total_failures = @bugs.where(:results => {:type => "failure"}).count
+    @total_skipped = @bugs.where(:results => {:type => "skipped"}).count
+
+    @bugs = @bugs.where(:results => {:type => params[:type]}) if params[:type].present?
+    @bugs = @bugs.page(params[:page])
     authorize!(:index, Case)
   end
   def show

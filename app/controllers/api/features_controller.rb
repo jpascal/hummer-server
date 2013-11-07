20edb6d4a9b3890ba6eb84acafc6ee71fd201f09
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-11-07 15:22:16 +0400

    uniques features in api features index

--- app/controllers/api/features_controller.rb
+++ app/controllers/api/features_controller.rb
@@ -1,6 +1,6 @@
 class Api::FeaturesController < Api::BaseController
   def index
-    render :json => ActsAsTaggableOn::Tag.joins(:taggings).where(:taggings => { :context => "features", :taggable_type => "Suite"}).as_json
+    render :json => ActsAsTaggableOn::Tag.joins(:taggings).where(:taggings => { :context => "features", :taggable_type => "Suite"}).uniq.as_json
   end
   def show
     render :json => ActsAsTaggableOn::Tag.joins(:taggings).where(:id => params[:id], :taggings => { :context => "features", :taggable_type => "Suite"}).first.as_json

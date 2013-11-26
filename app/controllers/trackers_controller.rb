5c07328581835a0403b1b842b408799af97929ec
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-11-26 18:13:01 +0400

    fix linking to trackers

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -5,14 +5,18 @@ class TrackersController < ApplicationController
   before_action :try_authorize
   respond_to :js
   def show
-    unless @case.tracker
+    unless @case.bug.present?
       render :edit
     end
   end
   def update
-    @case.tracker = params[:tracker].empty? ? nil : params[:tracker]
-    @case.tracker_user = current_user
-    @case.save!
+    bug = @case.bug || @case.build_bug(:name => @case.name, :classname => @case.classname, :user => current_user, :level => @case.result.type, :message => @case.result.message)
+    if params[:tracker].empty?
+      bug.destroy
+    else
+      bug.tracker = params[:tracker]
+      bug.save!
+    end
   end
 private
   def try_authorize

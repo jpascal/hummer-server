03bcf16c9563430a64d226599f6610fdd829be13
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-12-18 18:51:27 +0400

    makeup dashboard page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,7 @@
 class RootController < ApplicationController
   def index
+    @projects = Project.limit(5).order("created_at desc")
+    @suites = Suite.limit(5).order("created_at desc")
+    @bugs = Bug.limit(5).order("created_at desc")
   end
 end
